38f57815038c1fc22e36a748b7cc0c307934f0af
Jakub Tucek <jakub.tucek@gmail.com>
2020-09-07 22:34:44 +0200

    Prepare final test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('that that some currency is loaded', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const currencyElem = getByText(/Fiji Dollar/i);
+  expect(currencyElem).toBeInTheDocument();
+});
+
+test('that that currency is loaded based on query parameter', () => {
+  const newRelativePathQuery = window.location.pathname + '?currencySearch=Fiji Dollar';
+  window.history.pushState(null, '', newRelativePathQuery);
+
+  const { getByText, queryByText } = render(<App />);
+  const currencyElem = getByText(/Fiji Dollar/i);
+  expect(currencyElem).toBeInTheDocument();
+
+  const nonExistingCurrencyElem = queryByText(/CZK/i);
+  expect(nonExistingCurrencyElem).toBe(null);
+});
+
+test('test that currency can be search using input', () => {
+  const { getByText } = render(<App />);
+  const currencyElem = getBy(/Fiji Dollar/i);
+
 });
